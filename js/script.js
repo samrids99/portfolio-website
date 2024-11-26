d4137d515a43c42270e5f802080b96007e7547be
Samuel Ridsdale <sam.ridsdale@netpointsolutions.co.uk>
2024-11-26 13:31:59 +0000

    typing effect added with JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Add custom JavaScript here
+function runTypingEffect() {
+    const text = 'I am Sam Ridsdale';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 150;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+   for(let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+        typingElement.textContent += text.charAt(i);
+    }, delay * i);
+   }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
